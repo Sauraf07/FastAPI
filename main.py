3e117e955433d4f1fc49f0780dfaf38ba2e77c75
Sauraf Kumar <saurafsps7631@gmail.com>
2026-06-16 21:45:00 +0530

    Fast API

---
+++ main.py
@@ -0,0 +1,25 @@
+from urllib.request import Request
+#
+# from fastapi import FastAPI
+# app  = FastAPI
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.post("/greet")
+def greet_user(name: "Khushi"):
+    return {"message": "Hello, " + name + "!"}
+# @app.get("/")
+# def read_root():
+#     return {"Hello": "World"}
+# @app.get("/Khushi")
+# def read_root():
+#     return {"Hi Khushi"}
+# @app.get("/Saurav")
+# def read_root():
+#     return {"Saurav"}
+# @app.post("/")
+# def greet_user(name: str):
+#     name = "Khushi"
+#     return {"message": "Hello, " + name + "!"}
